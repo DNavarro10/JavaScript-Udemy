5bdee42f3801f4986030d9fd9bff71da952923f0
DNavarro10 <luis_d_100@hotmail.com>
2019-05-08 00:15:29 -0600

    avanzando

--- js/Practica-1.js
+++ js/Practica-1.js
@@ -4,4 +4,27 @@ document.write("<div class='contenedor'>");
 document.write("<h1>JavaScript - Udemy</h1><hr><hr class='hr2'>")
 document.write("</div>");
 
+/*
+1- pedir 6 muneros por pantalla - para array
+2- mostrar el array, en cuerpo y consola) 
+3- odernar y mostrar
+4- cuantos elementos tiene el array
+6- buscar valor introducido por el usuario. mostrar si esta y el indice.
+*/
 
+/* Array */
+
+let numeros = new Array(6);
+
+for (let i = 0; i < numeros.length; i++) {
+    numeros[i]  = parseInt(prompt("Introduce un numero: ", 0));  
+}
+/* Mostrar en pantalla */
+
+document.write("<h2>Contenido del Array</h2>");
+
+numeros.forEach((numero, i) =>{
+    document.write(`<strong> ${numero} </strong><hr>`);
+});
+/* Mostrar en consola*/ 
+console.log(numeros);
