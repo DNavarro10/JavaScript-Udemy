2a8f0512958bd30e0fc1304be59d292b04609c50
DNavarro10 <luis_d_100@hotmail.com>
2019-05-11 22:35:22 -0600

    validacion simple

--- js/Practica-1.js
+++ js/Practica-1.js
@@ -1,13 +1,33 @@
 'use strict'
 
-window.addEventListener('load', ( () =>{
+window.addEventListener('load',  function(){
     console.log("DOM cargado!!");
 
-    let formulario = document.querySelector(#formulario);
+    let form = document.querySelector("#formulario");
 
-    formulario.addEventListener('submit',(formulario) =>{
-        console.log("Evento Submit capturado")
 
+    form.addEventListener('submit', function(){
+        console.log("Evento Submit capturado");
+
+        let nombre = document.querySelector("#nombre").value;
+        let apellido = document.querySelector("#apellido").value;
+        let edad =  parseInt(document.querySelector("#edad").value);
+
+        // validar
+        if (nombre.trim() == null || nombre.trim().length == 0){
+            alert('nombre no valido')
+            return false;
+        }
+        
+        if (apellido.trim() == null || apellido.trim().length == 0){
+            alert('apellido no valido')
+            return false;
+        }
+
+        if (edad == null || edad.length >= 0 || !isNaN(edad) ){
+            alert('Edad no valida')
+            return false;
+        }
+        console.log(nombre,apellido,edad);
     });
-
-}));
+});
