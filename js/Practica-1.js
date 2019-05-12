e20f8661c02d4f726a9f6d048c298c257dae9185
DNavarro10 <luis_d_100@hotmail.com>
2019-05-11 21:44:12 -0600

    Formulario listo

--- js/Practica-1.js
+++ js/Practica-1.js
@@ -1,59 +1,13 @@
 'use strict'
-//html base
-document.write("<div class='contenedor'>");
-document.write("<h1>JavaScript - Udemy</h1><hr><hr class='hr2'>")
 
+window.addEventListener('load', ( () =>{
+    console.log("DOM cargado!!");
 
-/*
-1- pedir 6 muneros por pantalla - para array
-2- mostrar el array, en cuerpo y consola) 
-3- odernar y mostrar
-4- cuantos elementos tiene el array
-6- buscar valor introducido por el usuario. mostrar si esta y el indice.
-*/
+    let formulario = document.querySelector(#formulario);
 
-/* Mostrar en pantalla */
-// Texto, parametro para agregar textos
-function mostrarArray(elementos, texto = ""){
-    document.write(`<h2>Contenido del Array ${texto}</h2>`);
+    formulario.addEventListener('submit',(formulario) =>{
+        console.log("Evento Submit capturado")
 
-    document.write("<ul>");
-    numeros.forEach((elemento, i) =>{
-        document.write(`<li>${elemento}</li>`);
     });
-    document.write("</ul>");
-}
-/* Array */
 
-    document.write("<div class='array'>");
-    
-    let numeros = [];
-
-    for (let i = 0; i <= 5; i++) {
-            numeros.push(parseInt(prompt(`Introduce un numero: `,0))); 
-    }
-        
-
-    document.write("</div>");
-
-/* Mostrar en consola*/ 
-console.log(numeros);
-/* llamar funcion */
-mostrarArray(numeros);
-console.log(numeros);
-
-/* numeros ordenados */
-
-//numeros.sort(function(a,b){return a - b;});
-
-numeros.sort((a,b) => a-b); /* ES6 */
-mostrarArray(numeros, 'Ordenado');
-
-/* array invertido usando reverse */
-numeros.reverse();
-mostrarArray(numeros, 'Revertidos');
-
-/* contar elementos */
-
-document.write(`<h2>Tamaño del array ${numeros.length}</h2>`);
-document.write("</div>");
+}));
